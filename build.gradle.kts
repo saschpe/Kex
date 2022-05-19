1fb2c9f2e7d6cabbbb2b7b1c4e99cfe680a16e71
Sascha Peilicke <sascha@peilicke.de>
2022-05-19 12:59:08 +0200

    Add CHANGELOG.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.6.21"
     id("com.android.library") version "7.0.4" apply false
-    id("com.diffplug.spotless") version "6.5.1"
+    id("com.diffplug.spotless") version "6.6.1"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
 
