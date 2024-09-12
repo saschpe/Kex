911e44553e140c1f336d53938c58922b459120e9
Sascha Peilicke <sascha@peilicke.de>
2024-09-12 17:42:36 +0200

    Update dependency version plugin and refactor task configuration
    Upgraded com.github.ben-manes.versions plugin from version 0.42.0 to
    0.51.0 for better compatibility and features. Refactored
    DependencyUpdatesTask configuration to remove unnecessary nesting and
    simplify the Gradle file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.7.10" apply false
     id("com.android.library") version "7.2.2" apply false
     id("com.diffplug.spotless") version "6.25.0"
-    id("com.github.ben-manes.versions") version "0.42.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 spotless {
@@ -19,11 +19,9 @@ spotless {
     }
 }
 
-tasks {
-    withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
-        rejectVersionIf {
-            fun isStable(version: String) = Regex("^[0-9,.v-]+(-r)?$").matches(version)
-            !isStable(candidate.version) && isStable(currentVersion)
-        }
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        fun isStable(version: String) = Regex("^[0-9,.v-]+(-r)?$").matches(version)
+        !isStable(candidate.version) && isStable(currentVersion)
     }
 }
