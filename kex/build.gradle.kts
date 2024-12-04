ab84fe6330d61921a68f7f477a37a8ea09c4abb6
Sascha Peilicke <sascha@peilicke.de>
2024-12-04 14:55:15 +0100

    Update ktor-io version to 3.0.2 in build configuration.
    Upgrade the ktor-io library from version 3.0.1 to 3.0.2.
    
    See https://ktor.io/changelog/3.0/

--- kex/build.gradle.kts
+++ kex/build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation("io.ktor:ktor-io:3.0.1")
+            implementation("io.ktor:ktor-io:3.0.2")
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
